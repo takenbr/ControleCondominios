fa1bb6456586f7df2dea8537bd5d9b86723bc5fa
takenbr <taken_br@hotmail.com>
2021-04-10 22:27:58 -0300

    Adicionar arquivos de projeto.

---
+++ ControleCondominios/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ControleCondominios.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
